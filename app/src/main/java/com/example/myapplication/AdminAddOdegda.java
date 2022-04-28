3eb033bb26eb8bb68be6e7b52ef582898f88cb10
dreyv <dreyvania999@gmail.com>
2022-04-28 22:03:01 +0300

    admin

--- app/src/main/java/com/example/myapplication/AdminAddOdegda.java
+++ app/src/main/java/com/example/myapplication/AdminAddOdegda.java
@@ -2,13 +2,36 @@ package com.example.myapplication;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.media.Image;
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 
 public class AdminAddOdegda extends AppCompatActivity {
+
+    Button addposition, pageClother, pageReg;
+    TextView nameaddodegda, addimageodegda, addcostodegda, addsexogegda;
+    EditText nameadd, costadd, addsex;
+//    Image addimage;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_admin_add_odegda);
+        nameadd = findViewById(R.id.nameadd);
+        costadd = findViewById(R.id.costadd);
+        addsex = findViewById(R.id.addsex);
+        nameaddodegda = findViewById(R.id.nameaddodegda);
+        addimageodegda = findViewById(R.id.addimageodegda);
+        addcostodegda = findViewById(R.id.addcostodegda);
+        addsexogegda = findViewById(R.id.addsexogegda);
+        pageClother = findViewById(R.id.pageClother);
+        addposition = findViewById(R.id.addposition);
+        pageReg = findViewById(R.id.pageReg);
+//        pageClother.setOnClickListener(this);
+//        addposition.setOnClickListener(this);
+//        pageReg.setOnClickListener(this);
+
     }
 }
