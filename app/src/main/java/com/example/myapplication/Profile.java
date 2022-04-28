231ab64eea1b170c484635e89fd190857f21e0f7
dreyv <dreyvania999@gmail.com>
2022-04-28 22:15:49 +0300

    odegda

--- app/src/main/java/com/example/myapplication/Profile.java
+++ app/src/main/java/com/example/myapplication/Profile.java
@@ -3,12 +3,29 @@ package com.example.myapplication;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 
 public class Profile extends AppCompatActivity {
+
+    Button addposition, pageClother, pageReg;
+    TextView nameaddodegda, addimageodegda, addcostodegda, addsexogegda;
+    EditText nameadd, costadd, addsex;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_profile);
+        nameadd = findViewById(R.id.nameadd);
+        costadd = findViewById(R.id.costadd);
+        addsex = findViewById(R.id.addsex);
+        nameaddodegda = findViewById(R.id.nameaddodegda);
+        addimageodegda = findViewById(R.id.addimageodegda);
+        addcostodegda = findViewById(R.id.addcostodegda);
+        addsexogegda = findViewById(R.id.addsexogegda);
+        pageClother = findViewById(R.id.pageClother);
+        addposition = findViewById(R.id.addposition);
+        pageReg = findViewById(R.id.pageReg);
     }
 }
